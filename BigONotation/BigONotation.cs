38748d7c241dc51d058281d19a00a0beb5653adf
mollarc <mollark@live.com>
2023-09-17 23:54:31 -0500

    Comments Added

--- BigONotation/BigONotation.cs
+++ BigONotation/BigONotation.cs
@@ -14,11 +14,17 @@ namespace BigONotation
 
         }
 
+        /*
+         *This method is constant or O(1) because this method only requires one step regardless of input
+         */
         public void OConstant()
         {
             Console.WriteLine("Hello World!");
         }
 
+        /*
+         * This method runs in O(n) because it steps n times where n is a positive number
+         */
         public void OLinear(int n)
         {
             for(int i = 0; i < n; i++)
@@ -27,6 +33,9 @@ namespace BigONotation
             }
         }
 
+        /*
+         * This method runs in O(n^2) because the inner loop runs n1 times of n2 times of the outer loop if both n1 and n2 are postive numbers
+         */
         public void OQuadratic(int n1, int n2)
         {
             for(int i = 0;i < n1;i++)
